c45e6b71e211ef38b602dcd95803bf5932551b98
Shanting Hou <shantinghhou@gmail.com>
2025-04-01 20:32:39 -0400

    stop tracking node modules and dist

--- dist/components/AnimatedOnHover.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-type AnimatedOnHoverProps = {
-    src: React.ReactElement;
-    width?: number;
-    height?: number;
-    animation?: string;
-};
-declare const AnimatedOnHover: ({ src, width, height, animation, }: AnimatedOnHoverProps) => import("react/jsx-runtime").JSX.Element;
-export default AnimatedOnHover;
